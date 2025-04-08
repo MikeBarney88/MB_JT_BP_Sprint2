90c089311619a05f3a1070771e05460b43789a65
Joey <jowhiey@gmail.com>
2025-04-08 13:23:05 -0230

    joey created basic shoppingcart component that fetches from server (everyone pls do npm install)

--- React-App/nl_computers/src/components/ShoppingCart.jsx
+++ React-App/nl_computers/src/components/ShoppingCart.jsx
@@ -1,12 +1,33 @@
-
+import {useState, useEffect} from "react";
+import {Link} from "react-router-dom";
 
 function ShoppingCart() {
+    const [loading, setLoading] = useState(true);
+    const [cart, setCart] = useState();
 
 
-    return (
-        <div>
+    useEffect(()=>{
+        async function storeServerToCart() {
+            const res = await fetch("http://localhost:3000/cart");
 
+            const data = await res.json();
+
+            setCart(data);
+            setLoading(false);
+        }
+
+
+        storeServerToCart();
+    }, [])
+
+
+    return loading ? (<div className="cartBox"><li>Loading...</li></div>) : (
+        <div className="cartBox">
+            {cart.map((item, index)=>(
+                <li key={index}>{item.item1}</li>
+            ))}
         </div>
     )
 }
+
 export default ShoppingCart;
