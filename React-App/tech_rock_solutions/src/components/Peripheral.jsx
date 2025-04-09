57a12c34a477dc177a3f1b8f0b4676fc5ae4082d
Brandon Pike <Brandon.Pike@keyin.com>
2025-04-09 14:20:07 -0230

    i forgot dots

--- React-App/tech_rock_solutions/src/components/Peripheral.jsx
+++ React-App/tech_rock_solutions/src/components/Peripheral.jsx
@@ -1,19 +1,32 @@
-import CorsairHS70 from "/images/CorsairHS70.jpg"
-import CosairK70 from "/images/CosairK70.jpg"
-import CorScim from "/images/CorScim.jpg"
-import LogitechG502 from "/images/LogitechG502.jpg"
-import LogitechMK370 from "/images/LogitechMK370.jpg"
-import RazerBWidow from "/images/RazerBWidow.jpg"
-import RazerKitty from "/images/RazerKitty.jpg"
-import RazerNaga from "/images/RazerNaga.jpg"
-import ShuresSM7B from "/images/ShuresSM7B.jpg"
+import CorsairHS70 from "../images/CorsairHS70.jpg"
+import CosairK70 from "/..images/CosairK70.jpg"
+import CorScim from "/..images/CorScim.jpg"
+import LogitechG502 from "/..images/LogitechG502.jpg"
+import LogitechMK370 from "/..images/LogitechMK370.jpg"
+import RazerBWidow from "/..images/RazerBWidow.jpg"
+import RazerKitty from "/..images/RazerKitty.jpg"
+import RazerNaga from "/..images/RazerNaga.jpg"
+import ShuresSM7B from "/..images/ShuresSM7B.jpg"
 
 
 
 
 function Peripheral() {
   return (
-    <div>Peripheral</div>
+    <>
+    <div className=" Peripherals-header">
+    <h1>The Best Computer Peripherals</h1>
+    </div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    </>
   )
 }
 export default Peripheral
