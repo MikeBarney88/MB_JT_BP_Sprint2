b87a81e5c031eb2b990d45e3d355d3c882e5d408
Joey <jowhiey@gmail.com>
2025-04-09 13:43:37 -0230

    joey set up routes

--- React-App/nl_computers/src/App.jsx
+++ React-App/nl_computers/src/App.jsx
@@ -1,5 +1,5 @@
-desc: Sprint2;
-Author: Joey, Michael, Brandon;
+//desc: Sprint2;
+//Author: Joey, Michael, Brandon;
 
 import Logo from "./Images/Logo.jpg";
 import Prebuilt1 from "./Images/Prebuilt1.webp";
@@ -13,7 +13,7 @@ import Prebuilt8 from "./Images/Prebuilt8.webp";
 import Prebuilt9 from "./Images/Prebuilt9.png";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import ShoppingCart from "./Components/ShoppingCart";
+import ShoppingCart from "./components/ShoppingCart";
 import { Link } from "react-router-dom";
 
 function App() {
@@ -25,116 +25,119 @@ function App() {
         <br />
         <br />
         <nav>
-          <Routes>
-            <Route path="/cart" element={<ShoppingCart />} />
-            {/* <Route path="/accessories" element={}/>
-          <Route path="/details" element={}/> */}
-          </Routes>
+
         </nav>
       </header>
+        <Routes>
+          <Route path="/" element={
+            <main>
+              <div className=" pc-header">
+                <h1>Ready to Ship Computers</h1>
+              </div>
 
-      <main>
-        <div className=" pc-header">
-          <h1>Ready to Ship Computers</h1>
-        </div>
+              <div className="card pc-card1">
+                <img src={Prebuilt1} width={180} height={180} alt="Pc1" />
 
-        <div className="card pc-card1">
-          <img src={Prebuilt1} width={180} height={180} alt="Pc1" />
+                <br />
+                <h3 className="card-h3">Dildo</h3>
+                <p>Ryzen 7 5700X3D | RTX 4070 Super | 32GB RAM | 2TB SSD</p>
+                <br />
+                <h3 className="card-h3">$1999.99</h3>
 
-          <br />
-          <h3 className="card-h3">Dildo</h3>
-          <p>Ryzen 7 5700X3D | RTX 4070 Super | 32GB RAM | 2TB SSD</p>
-          <br />
-          <h3 className="card-h3">$1999.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card2">
+                <img src={Prebuilt2} width={180} height={180} alt="Pc2" />
+                <br />
+                <h3 className="card-h3">Quidi Vidi</h3>
+                <p>Ryzen 7 7800X3D | Radeon RX 9070 | 32GB RAM | 2TB SSD</p>
+                <h3 className="card-h3">$2399.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-        <div className="card pc-card2">
-          <img src={Prebuilt2} width={180} height={180} alt="Pc2" />
-          <br />
-          <h3 className="card-h3">Quidi Vidi</h3>
-          <p>Ryzen 7 7800X3D | Radeon RX 9070 | 32GB RAM | 2TB SSD</p>
-          <h3 className="card-h3">$2399.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card3">
+                <img src={Prebuilt3} width={180} height={180} alt="Pc3" />
+                <br />
+                <h3 className="card-h3">Twillingate</h3>
+                <p>Intel Core i7-13700F | RTX 4070 Super | 32GB RAM | 1TB SSD</p>
+                <h3 className="card-h3">$2599.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-        <div className="card pc-card3">
-          <img src={Prebuilt3} width={180} height={180} alt="Pc3" />
-          <br />
-          <h3 className="card-h3">Twillingate</h3>
-          <p>Intel Core i7-13700F | RTX 4070 Super | 32GB RAM | 1TB SSD</p>
-          <h3 className="card-h3">$2599.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card4">
+                <img src={Prebuilt4} width={180} height={180} alt="Pc4" />
+                <br />
+                <h3 className="card-h3">CBS</h3>
+                <p>Ryzen 7 7800X3D | RTX 4070 Super | 32GB RAM | 1TB SSD</p>
+                <br />
+                <h3 className="card-h3">$2899.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-        <div className="card pc-card4">
-          <img src={Prebuilt4} width={180} height={180} alt="Pc4" />
-          <br />
-          <h3 className="card-h3">CBS</h3>
-          <p>Ryzen 7 7800X3D | RTX 4070 Super | 32GB RAM | 1TB SSD</p>
-          <br />
-          <h3 className="card-h3">$2899.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card5">
+                <img src={Prebuilt5} width={180} height={180} alt="Pc5" />
+                <br />
+                <h3 className="card-h3">Burin</h3>
+                <p>- Ryzen 7 9800X3D | RTX 4080 Super | 64GB RAM | 1TB + 2TB SSD</p>
+                <h3 className="card-h3">$2999.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-        <div className="card pc-card5">
-          <img src={Prebuilt5} width={180} height={180} alt="Pc5" />
-          <br />
-          <h3 className="card-h3">Burin</h3>
-          <p>- Ryzen 7 9800X3D | RTX 4080 Super | 64GB RAM | 1TB + 2TB SSD</p>
-          <h3 className="card-h3">$2999.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card6">
+                <img src={Prebuilt6} width={180} height={180} alt="Pc6" />
+                <br />
+                <h3 className="card-h3">Springdale</h3>
+                <p>AMD Ryzen 7 7700 | RTX 4070 | 32GB RAM | 1TB SSD</p>
+                <br />
+                <h3 className="card-h3">$3199.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-        <div className="card pc-card6">
-          <img src={Prebuilt6} width={180} height={180} alt="Pc6" />
-          <br />
-          <h3 className="card-h3">Springdale</h3>
-          <p>AMD Ryzen 7 7700 | RTX 4070 | 32GB RAM | 1TB SSD</p>
-          <br />
-          <h3 className="card-h3">$3199.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card7">
+                <img src={Prebuilt7} width={180} height={180} alt="Pc7" />
+                <br />
+                <h3 className="card-h3">Corner Brook</h3>
+                <p>
+                  Intel Core i9-14900KF | RTX 4080 Super | 64GB RAM | 2TB + 2TB SSD
+                </p>
 
-        <div className="card pc-card7">
-          <img src={Prebuilt7} width={180} height={180} alt="Pc7" />
-          <br />
-          <h3 className="card-h3">Corner Brook</h3>
-          <p>
-            Intel Core i9-14900KF | RTX 4080 Super | 64GB RAM | 2TB + 2TB SSD
-          </p>
+                <h3 className="card-h3">$3399.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-          <h3 className="card-h3">$3399.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card8">
+                <img src={Prebuilt8} width={180} height={180} alt="Pc8" />
+                <br />
+                <h3 className="card-h3">St. John's</h3>
+                <p>Ryzen 7 7800X3D | RTX 4080 Super | 32GB RAM | 1TB + 2TB SSD</p>
 
-        <div className="card pc-card8">
-          <img src={Prebuilt8} width={180} height={180} alt="Pc8" />
-          <br />
-          <h3 className="card-h3">St. John's</h3>
-          <p>Ryzen 7 7800X3D | RTX 4080 Super | 32GB RAM | 1TB + 2TB SSD</p>
+                <h3 className="card-h3">$3799.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-          <h3 className="card-h3">$3799.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="card pc-card9">
+                <img src={Prebuilt9} width={180} height={180} alt="Pc9" />
+                <br />
+                <h3 className="card-h3">Cape Spear</h3>
+                <p>Ryzen 9 7800X3D | RTX 5090 Ti Super | 64GB RAM | 2TB + 2TB M.2</p>
 
-        <div className="card pc-card9">
-          <img src={Prebuilt9} width={180} height={180} alt="Pc9" />
-          <br />
-          <h3 className="card-h3">Cape Spear</h3>
-          <p>Ryzen 9 7800X3D | RTX 5090 Ti Super | 64GB RAM | 2TB + 2TB M.2</p>
+                <h3 className="card-h3">$5999.99</h3>
+                <button id="pc-button">Add to Cart</button>
+              </div>
 
-          <h3 className="card-h3">$5999.99</h3>
-          <button id="pc-button">Add to Cart</button>
-        </div>
+              <div className="about-header">
+                <h2>About Us</h2>
+              </div>
+            </main>
+          }/>
 
-        <div className="about-header">
-          <h2>About Us</h2>
-        </div>
-      </main>
+          <Route path="/cart" element={<ShoppingCart />} />
+          {/* <Route path="/accessories" element={}/>
+        <Route path="/details" element={}/> */}
+        </Routes>
 
-      <footer></footer>
+        <footer></footer>
     </Router>
   );
 }
