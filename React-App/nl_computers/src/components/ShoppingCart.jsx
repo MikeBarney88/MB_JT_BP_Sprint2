fd0e6931482b4047232c7b33d68fcd94cec9b79f
Joey <jowhiey@gmail.com>
2025-04-09 13:08:19 -0230

    joey added cart more functionality yayyyyyyy

--- React-App/nl_computers/src/components/ShoppingCart.jsx
+++ React-App/nl_computers/src/components/ShoppingCart.jsx
@@ -1,31 +1,18 @@
-import {useState, useEffect} from "react";
-import {Link} from "react-router-dom";
+import {useContext} from "react";
+import {ShoppingCartContext} from "../context/conShoppingCart";
 
 function ShoppingCart() {
-    const [loading, setLoading] = useState(true);
-    const [cart, setCart] = useState();
+    const cart = useContext(ShoppingCartContext);
 
+    return (
+        <div className="shoppingCartInfo">
+            <h1>Your Shopping Cart:</h1>
 
-    useEffect(()=>{
-        async function storeServerToCart() {
-            const res = await fetch("http://localhost:3000/cart");
-
-            const data = await res.json();
-
-            setCart(data);
-            setLoading(false);
-        }
-
-
-        storeServerToCart();
-    }, [])
-
-
-    return loading ? (<div className="cartBox"><li>Loading...</li></div>) : (
-        <div className="cartBox">
-            {cart.map((item, index)=>(
-                <li key={index}>{item.item1}</li>
-            ))}
+            <div className="cartBox">
+                {cart.loading ? (<li>Loading...</li>) : cart.cart.map((item, index)=>(
+                    <li key={index}>{item.item1} <button onClick={(e)=>{removeFromCart(e.target.id)}}>Remove</button></li>
+                ))}
+            </div>
         </div>
     )
 }
