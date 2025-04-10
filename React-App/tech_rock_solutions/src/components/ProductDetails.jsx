0340431366be5191e0b402209c96ab5addc5d20e
MikeBarney88 <michaelbarney1988@gmail.com>
2025-04-10 14:10:25 -0230

    final thursday

--- React-App/tech_rock_solutions/src/components/ProductDetails.jsx
+++ React-App/tech_rock_solutions/src/components/ProductDetails.jsx
@@ -1,5 +1,36 @@
 const ProductDetails = () => {
-  return;
-  <div></div>;
+  return (
+    <main>
+      <div className="card product-card">
+        <img src={null} alt="" />
+        <br />
+        <h3>Pc Name:</h3>
+        <br />
+        <h1>Specs</h1>
+        <br />
+        <hr />
+        <h4 className="h4-left">Operating System: </h4>
+        <hr />
+        <h4 className="h4-left">Processor: </h4>
+        <hr />
+        <h4 className="h4-left">Mother Board: </h4>
+        <hr />
+        <h4 className="h4-left">Graphic Card: </h4>
+        <hr />
+        <h4 className="h4-left">Memory: </h4>
+        <hr />
+        <h4 className="h4-left">Hard Drive:</h4>
+        <hr />
+        <h4 className="h4-left">USB Ports:</h4>
+        <hr />
+        <h4 className="h4-left">Connectivity:</h4>
+        <hr />
+        <br />
+        <h2>Cost:</h2>
+        <br />
+        <button id="spec-button">Add to Cart</button>
+      </div>
+    </main>
+  );
 };
 export default ProductDetails;
