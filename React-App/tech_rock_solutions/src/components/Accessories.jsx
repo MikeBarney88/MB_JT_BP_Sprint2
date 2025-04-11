a63c23130e044bf618b1c249557c6a225f4b2a77
Brandon Pike <Brandon.Pike@keyin.com>
2025-04-11 12:05:31 -0230

    pretty pictures

--- React-App/tech_rock_solutions/src/components/Accessories.jsx
+++ React-App/tech_rock_solutions/src/components/Accessories.jsx
@@ -1,9 +1,9 @@
 import CorsairHS70 from "../images/CorsairHS70.avif";
 import CorsairK70 from "../images/CorsairK70.avif";
-import CorScim from "../images/CorScim.jpg";
-import LogitechG502 from "../images/LogitechG502.jpg";
-import LogitechMK370 from "../images/LogitechMK370.jpg";
-import RazerBWidow from "../images/RazerBWidow.jpg";
+import CorsairScim from "../images/CorsairScim.avif";
+import LogitechYeti from "../images/LogitechYeti.avif";
+import LogitechK750 from "../images/LogitechK750.avif";
+import LogitechG502 from "../images/LogitechG502.avif";
 import RazerKitty from "../images/RazerKitty.jpg";
 import RazerNaga from "../images/RazerNaga.jpg";
 import ShureSM7B from "../images/ShureSM7B.jpg";
@@ -41,7 +41,7 @@ function Accessories() {
         </div>
 
         <div className="card pc-card3">
-          <img src={CorScim} width={180} height={180} alt="Pc3" />
+          <img src={CorsairScim} width={180} height={180} alt="Pc3" />
 
           <br />
           <h3 className="card-h3"></h3>
@@ -53,23 +53,23 @@ function Accessories() {
         </div>
 
         <div className="card pc-card4">
-          <img src={LogitechG502} width={180} height={180} alt="Pc4" />
+          <img src={LogitechYeti} width={180} height={180} alt="Pc4" />
 
           <br />
           <h3 className="card-h3"></h3>
-          <p>Logitech G502</p>
+          <p>Logitech Yeti</p>
           <br />
-          <h3 className="card-h3">$124.99</h3>
+          <h3 className="card-h3">$424.99</h3>
 
           <button id="pc-button">Add to Cart</button>
         </div>
 
         <div className="card pc-card5">
-          <img src={LogitechMK370} width={180} height={180} alt="Pc5" />
+          <img src={LogitechK750} width={180} height={180} alt="Pc5" />
 
           <br />
           <h3 className="card-h3"></h3>
-          <p>Logitech MK370</p>
+          <p>Logitech K750</p>
           <br />
           <h3 className="card-h3">$100.00</h3>
 
@@ -77,11 +77,11 @@ function Accessories() {
         </div>
 
         <div className="card pc-card6">
-          <img src={RazerBWidow} width={180} height={180} alt="Pc6" />
+          <img src={LogitechG502} width={180} height={180} alt="Pc6" />
 
           <br />
           <h3 className="card-h3"></h3>
-          <p>Razer Black Widow</p>
+          <p>Logitech G502</p>
           <br />
           <h3 className="card-h3">$200.00</h3>
 
