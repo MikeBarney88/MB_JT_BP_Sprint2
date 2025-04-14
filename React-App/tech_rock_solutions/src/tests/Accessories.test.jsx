90230cb2c82576eff3f40b87a1a6f8c271320ad2
MikeBarney88 <michaelbarney1988@gmail.com>
2025-04-14 13:44:42 -0230

    added testing libs

---
+++ React-App/tech_rock_solutions/src/tests/Accessories.test.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import Accessories from "../components/Accessories";
+import { test, expect, describe } from "vitest";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom/vitest";
