a73c520233514d31af83661d108e1b6da614ff7f
Brandon Pike <Brandon.Pike@keyin.com>
2025-04-16 13:34:57 -0230

    testing head?

--- React-App/tech_rock_solutions/src/tests/Accessories.test.jsx
+++ React-App/tech_rock_solutions/src/tests/Accessories.test.jsx
@@ -3,3 +3,19 @@ import Accessories from "../components/Accessories";
 import { test, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
+
+describe('Accessories', () => {
+    render(<Accessories />);
+    test('renders accessories list', () => {
+      
+      expect(screen.getByText('Razer Naga')).toBeInTheDocument();
+      expect(screen.getByText('Razer Kraken Kitty')).toBeInTheDocument();
+      expect(screen.getByText('Razer Black Widow')).toBeInTheDocument();
+    });
+    test('renders the Accessories page', () => {
+  
+      const headerElement = screen.getByRole('Header')
+      expect(headerElement).toBeInTheDocument();
+    });
+})
+
